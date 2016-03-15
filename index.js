0f9884c9b8f6677301c46956a4fd1f0c670b656f
Autarc <autarc@gmail.com>
2016-03-15 13:51:03 +0100

    remove unused argument

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var electron = require('electron-eval')
 
-module.exports = function (cb) {
+module.exports = function () {
   var daemon = electron()
   return {
     electronDaemon: daemon,
-    RTCPeerConnection: require('./src/RTCPeerConnection.js')(daemon, cb),
+    RTCPeerConnection: require('./src/RTCPeerConnection.js')(daemon),
     RTCSessionDescription: require('./src/RTCSessionDescription.js'),
     RTCIceCandidate: require('./src/RTCIceCandidate.js'),
     RTCDataChannel: require('./src/RTCDataChannel.js')
