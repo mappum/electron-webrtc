aa04fdedea6c939c400415340d336b2140b06d37
Matt Bell <mappum@gmail.com>
2016-03-15 14:01:55 -0700

    Added test for error delegation

---
+++ test/errors.js
@@ -0,0 +1,18 @@
+var test = require('tap').test
+
+var wrtc
+test('create daemon', (t) => {
+  wrtc = require('..')()
+  wrtc.electronDaemon.once('ready', t.end)
+})
+
+test('create RTCPeerConnection after close', (t) => {
+  wrtc.on('error', (err) => {
+    t.ok(err, 'error emitted')
+    t.end()
+  })
+
+  wrtc.electronDaemon.close()
+  var pc = new wrtc.RTCPeerConnection()
+  pc
+})
