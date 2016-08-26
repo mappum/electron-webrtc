182dbd916871a7fdce5654da55f75d9b19230ff9
Matt Bell <mappum@gmail.com>
2016-08-26 13:34:48 -0700

    Re-enabled 'trickle: false' test

--- test/simple-peer.js
+++ test/simple-peer.js
@@ -223,55 +223,52 @@ test('duplex stream: send data one-way', function (t) {
 // test/trickle.js
 
 test('disable trickle', function (t) {
-  t.pass('SKIPPING')
-  t.end()
+  var peer1 = new Peer({ initiator: true, trickle: false, wrtc })
+  var peer2 = new Peer({ trickle: false, wrtc })
 
-  // var peer1 = new Peer({ initiator: true, trickle: false, wrtc })
-  // var peer2 = new Peer({ trickle: false, wrtc })
-  //
-  // var numSignal1 = 0
-  // peer1.on('signal', function (data) {
-  //   numSignal1 += 1
-  //   peer2.signal(data)
-  // })
-  //
-  // var numSignal2 = 0
-  // peer2.on('signal', function (data) {
-  //   numSignal2 += 1
-  //   peer1.signal(data)
-  // })
-  //
-  // peer1.on('connect', tryTest)
-  // peer2.on('connect', tryTest)
-  //
-  // function tryTest () {
-  //   if (!peer1.connected || !peer2.connected) return
-  //
-  //   t.equal(numSignal1, 1, 'only one `signal` event')
-  //   t.equal(numSignal2, 1, 'only one `signal` event')
-  //   t.equal(peer1.initiator, true, 'peer1 is initiator')
-  //   t.equal(peer2.initiator, false, 'peer2 is not initiator')
-  //
-  //   peer1.send('sup peer2')
-  //   peer2.on('data', function (data) {
-  //     t.equal(data.toString(), 'sup peer2', 'got correct message')
-  //
-  //     peer2.send('sup peer1')
-  //     peer1.on('data', function (data) {
-  //       t.equal(data.toString(), 'sup peer1', 'got correct message')
-  //
-  //       function tryDone () {
-  //         if (!peer1.connected && !peer2.connected) {
-  //           t.pass('both peers closed')
-  //           t.end()
-  //         }
-  //       }
-  //
-  //       peer1.destroy(tryDone)
-  //       peer2.destroy(tryDone)
-  //     })
-  //   })
-  // }
+  var numSignal1 = 0
+  peer1.on('signal', function (data) {
+    numSignal1 += 1
+    peer2.signal(data)
+  })
+
+  var numSignal2 = 0
+  peer2.on('signal', function (data) {
+    numSignal2 += 1
+    peer1.signal(data)
+  })
+
+  peer1.on('connect', tryTest)
+  peer2.on('connect', tryTest)
+
+  function tryTest () {
+    if (!peer1.connected || !peer2.connected) return
+
+    t.equal(numSignal1, 1, 'only one `signal` event')
+    t.equal(numSignal2, 1, 'only one `signal` event')
+    t.equal(peer1.initiator, true, 'peer1 is initiator')
+    t.equal(peer2.initiator, false, 'peer2 is not initiator')
+
+    peer1.send('sup peer2')
+    peer2.on('data', function (data) {
+      t.equal(data.toString(), 'sup peer2', 'got correct message')
+
+      peer2.send('sup peer1')
+      peer1.on('data', function (data) {
+        t.equal(data.toString(), 'sup peer1', 'got correct message')
+
+        function tryDone () {
+          if (!peer1.connected && !peer2.connected) {
+            t.pass('both peers closed')
+            t.end()
+          }
+        }
+
+        peer1.destroy(tryDone)
+        peer2.destroy(tryDone)
+      })
+    })
+  }
 })
 
 test('disable trickle (only initiator)', function (t) {
